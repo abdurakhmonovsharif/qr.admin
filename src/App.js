db52c8d12d32b3783b997c9c22d0465acf069fba
Abdurakhmonov Sharif <115024917+abdurakhmonovsharif@users.noreply.github.com>
2024-03-21 01:31:37 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ import "./assets/styles/responsive.css";
 import Products from "./pages/Products";
 import Themes from "./pages/Themes";
 import { useEffect } from "react";
-export const base_url = "http://localhost:8080/api/";
+export const base_url = "https://qr-me-backend.onrender.com/api/";
 export const client_base_url = "https://qr-me-frontend.onrender.com/";
 // https://qr-me-backend.onrender.com/api/
 function App() {
